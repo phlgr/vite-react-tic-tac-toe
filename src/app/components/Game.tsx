20839f8b760a46850e4327d9e294be9acafc9e0a
phlgr <philipp@gartz.dev>
2021-08-16 15:35:59 +0200

    ✨ Insert Tic Tac Toe Tutorial Code

---
+++ src/app/components/Game.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Board from './Board';
+
+function Game(): JSX.Element {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/app/components/Square.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Square(): JSX.Element {
+  return <button className="square">{/* TODO */}</button>;
+}
+
+export default Square;
